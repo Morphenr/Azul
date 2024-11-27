e816ee01bcd82b649c5493433a6d90f03d603123
Marten Reichow <marten.reichow@schroders.com>
2024-11-27 23:48:38 +0100

    Evolutionary Algo

--- ml/DQN_class.py
+++ ml/DQN_class.py
@@ -6,11 +6,11 @@ class DQN(nn.Module):
         self.output_dim = output_dim
         super(DQN, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(input_dim, 128),
+            nn.Linear(input_dim, 64),
             nn.ReLU(),
-            nn.Linear(128, 64),
+            nn.Linear(64, 16),
             nn.ReLU(),
-            nn.Linear(64, output_dim)
+            nn.Linear(16, output_dim)
         )
 
     def forward(self, x):
